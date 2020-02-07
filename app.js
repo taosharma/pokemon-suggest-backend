270fdd9bfac783c3966716daa01f0c41a22a656a
Tao Sharma <taosharma@gmail.com>
2020-02-07 11:13:26 +0000

    retrieved all pokemon

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+app.get("/", async (request, response) => {
+  response.send("all good");
+});
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
